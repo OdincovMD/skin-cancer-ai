f637c7275a6b79e14f1d3ad9fd70e70eb9c96756
Tarasova Uliana <tarasova.ulya@gmail.com>
2024-10-26 16:12:54 +0300

    restructured 3 function to one, because it had no sence. Rewrite docsting for main, for classes of classification

--- several_lines_parallel_borozd_or_grebesh.py
+++ several_lines_parallel_borozd_or_grebesh.py
@@ -9,7 +9,6 @@ INPUT_SIZE = 224
 MEAN = [0.485, 0.456, 0.406]
 STD = [0.229, 0.224, 0.225]
 
-# глобальная по сути константа, должна быть с большой буквы
 DATA_TRANSFORMS = {
     'test': transforms.Compose([
         transforms.Resize(INPUT_SIZE),
@@ -18,7 +17,6 @@ DATA_TRANSFORMS = {
         transforms.Normalize(MEAN, STD)
     ]),
 }
-
 
 def load_model(model_path: str) -> Tuple[EfficientNet, torch.device]:
     """
@@ -38,48 +36,11 @@ def load_model(model_path: str) -> Tuple[EfficientNet, torch.device]:
     model.eval()
     return model, device
 
-
-def preprocess_image(img: np.ndarray) -> torch.Tensor:
-    """
-    Processes an image for prediction with the model.
-
-    Args:
-        img (np.ndarray): Original image in BGR.
-
-    Returns:
-        torch.Tensor: Image tensor prepared for input to the model.
-    """
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = img.transpose((2, 0, 1))
-    img = torch.tensor(img, dtype=torch.float) / 255.0
-    img = transforms.ToPILImage()(img.permute(1, 2, 0).to(torch.uint8).numpy())
-    img = DATA_TRANSFORMS['test'](img).unsqueeze(0)
-    return img
+# TODO: переименовать весовой файл several_parallel_lines_borozd_grebesh.pth') в соответствии с названием модуля -> several_lines_parallel_borozd_or_grebesh.pth
+MODEL, DEVICE = load_model('weight/several_parallel_lines_borozd_grebesh.pth')
 
 
-def predict(model: EfficientNet, device: torch.device, image_transform: torch.Tensor) -> str:
-    """
-    Makes a prediction on the class of the image using the model.
-
-    Args:
-        model (EfficientNet): The loaded model.
-        device (torch.device): Device for computation.
-        image_transform (torch.Tensor): Processed image tensor.
-
-    Returns:
-        str: Predicted class name.
-    """
-    with torch.no_grad():
-        prediction = model(image_transform.to(device))
-    return "Borozd" if torch.argmax(prediction) == 0 else "Grebesh"
-
-
-model, device = load_model('weight/several_parallel_lines_borozd_grebesh.pth')
-# если это опять глобальное, то почему мы с lower case обозвали переменную 
-# и еще и не передаем ее в функцию. BAd
-# TODO: сделать из этого global и передавать в функцию или сделать функцию на загрузку модели (как будто муторно)
-# TODO: переименовать весовой файл в соответствии с названием модуля -> several_lines_parallel_borozd_or_grebesh.pth
-
+# до этого main была функцией для вызова двух функций
 def main(img: np.ndarray) -> str:
     """
     The main function to conduct the entire process of loading, preprocessing, and predicting an image.
@@ -88,13 +49,23 @@ def main(img: np.ndarray) -> str:
         img (np.ndarray): image for preprocessing.
 
     Returns:
-        str: Result of the model prediction.
+        str: "Борозды" or "Гребешки"
     """
-    image_transform = preprocess_image(img)
-    prediction = predict(model, device, image_transform)
-    return prediction
+    # До этого это была функция для обработки
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.transpose((2, 0, 1))
+    img = torch.tensor(img, dtype=torch.float) / 255.0
+    img = transforms.ToPILImage()(img.permute(1, 2, 0).to(torch.uint8).numpy())
+    image_transform = DATA_TRANSFORMS['test'](img).unsqueeze(0)
+
+    # раньше была функция для предсказания
+    with torch.no_grad():
+        prediction = MODEL(image_transform.to(DEVICE))
+
+    return "Борозды" if torch.argmax(prediction) == 0 else "Гребешки"
 
 
+#выпилится после тестирования
 if __name__ == "__main__":
     img_path = '26.jpg'
     img = cv2.imread(img_path)
